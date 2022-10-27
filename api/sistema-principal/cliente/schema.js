34dde67d6249c1842ef039701737267511712dc8
Fabio Alderete <fabioalderete@MacBook-Pro-de-Fabio.local>
2022-10-27 13:57:49 -0300

    arreglos pocos

--- api/sistema-principal/cliente/schema.js
+++ api/sistema-principal/cliente/schema.js
@@ -4,22 +4,22 @@ const { required } = require("joi")
 const clienteSchemaCrear = Joi.object({
     nombre: Joi.string().required().min(2).max(40),
     apellidos: Joi.string().required().min(2).max(40),
-    direccion: Joi.string().allow(' ',null).min(6).max(20),
+    direccion: Joi.string().allow('',null).min(6).max(20),
     celular:Joi.number().required(),
     rut:Joi.string().min(9).max(12).required(),
-    pais:Joi.string().required().min(5).max(20),
-    ciudad:Joi.string().required().min(5).max(20),
+    pais:Joi.string().required().min(3).max(20),
+    ciudad:Joi.string().required().min(3).max(20),
     estado:Joi.number().required().min(0).max(1)
 })
 
 const clienteSchemaEditar = Joi.object({
     nombre: Joi.string().required().min(2).max(40),
     apellidos: Joi.string().required().min(2).max(40),
-    direccion: Joi.string().allow(' ',null).min(6).max(20),
+    direccion: Joi.string().allow('',null).min(6).max(20),
     celular:Joi.number().required(),
     rut:Joi.string().min(9).max(12).required(),
-    pais:Joi.string().required().min(5).max(20),
-    ciudad:Joi.string().required().min(5).max(20)
+    pais:Joi.string().required().min(3).max(20),
+    ciudad:Joi.string().required().min(3).max(20)
 })
 
 
